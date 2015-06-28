32132f666ff0b2567da4c3fc103710ebeb67ec34
Tomas Linhart <tomas@linhart.me>
2015-06-28 18:42:56 +0200

    Add project skeleton generated by Xcode

---
+++ SwiftGtk/SwiftGtk.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGtk.h
+//  SwiftGtk
+//
+//  Created by Tomas Linhart on 28/06/15.
+//  Copyright © 2015 Tomas Linhart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftGtk.
+FOUNDATION_EXPORT double SwiftGtkVersionNumber;
+
+//! Project version string for SwiftGtk.
+FOUNDATION_EXPORT const unsigned char SwiftGtkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGtk/PublicHeader.h>
+
+
