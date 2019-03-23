f99f2ac9a3d8d63cd4753628f82768ad9ec44c69
Vlady Aaron <vla9659@gmail.com>
2019-03-23 16:23:23 +0200

    Upgrading to Swift 4.2 (a project doesn't work yet)

--- Package.swift
+++ Package.swift
@@ -1,17 +1,4 @@
 import PackageDescription
 
-#if os(OSX)
-    let gtkUrl = "https://github.com/TomasLinhart/CGtk-OSX"
-#elseif os(Linux)
-    let gtkUrl = "https://github.com/TomasLinhart/CGtk-Linux"
-#else
-    fatalError("Unsupported platform.")
-#endif
-
 let package = Package(
-  name:  "SwiftGtk",
-  dependencies: [
-    .Package(url: gtkUrl, majorVersion: 1, minor: 3)
-  ],
-  exclude: ["Sources/Ancillary Files"]
-)
+        name: "sGtk")
---
+++ Package@swift-4.2.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:4.2
+
+import PackageDescription
+
+
+let package = Package(
+        name: "sGtk",
+        products: [
+            .library(name: "sGtk", targets: ["sGtk"])
+        ], //is necessary here?
+        targets: [
+            .target(
+                    name: "sGtk",
+                    dependencies: ["CGtk"]),
+            .systemLibrary(
+                    name: "CGtk",
+                    path: "Sources/CGtk",
+                    pkgConfig: "gtk+-3.0",
+                    providers: [.apt(["gtk3.0-dev"]),
+                                .brew(["gtk3.0-dev"])]), //not Tested yet
+        ]
+
+)
