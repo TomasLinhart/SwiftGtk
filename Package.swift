00499c3fb7859cd3fe2fcd1fcb2bc79e7412533e
Vlady Aaron <vla9659@gmail.com>
2019-03-27 23:14:58 +0200

    Update to swift 5.0. Tested on Ubuntu and macOS

--- Package.swift
+++ Package.swift
@@ -1,6 +1,36 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
+#if os(OSX)
+let gtkPath = "Sources/CGtk-Mac"
+#elseif os(Linux)
+let gtkPath = "Sources/CGtk-Linux"
+#else
+fatalError("Unsupported platform.")
+#endif
 let package = Package(
-        name: "sGtk")
+    name: "sGtk",
+    products: [
+        .library(name: "sGtk",
+                 targets: ["Gtk"]
+        )
+        
+        
+    ], //is necessary here?
+    targets: [
+        .systemLibrary(
+            name: "CGtk",
+            path: gtkPath,
+            pkgConfig: "gtk+-3.0",
+            providers: [.apt(["gtk3.0-dev"]),
+                        .brew(["pkg-config", "gtk+3"])]),
+
+        .target(
+            name: "Gtk",
+            dependencies: ["CGtk"],
+            path: "Sources/sGtk"
+        ),
+    ]
+    
+)
--- Sources/CGtk/header.h
+++
@@ -1,5 +0,0 @@
-//
-// Created by GothStar on 23.03.19.
-//
-
-#include <termios.h>
