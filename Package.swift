e5030aa8dbecc38aa27460d3a169dcd01eb3b783
Mathieu Barnachon <mathieu@styleshoots.com>
2022-01-14 15:49:40 +0100

    Export library

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGtk",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwiftGtk",
+            targets: ["SwiftGtk"]),
+    ],
     dependencies: [],
     targets: [
         .systemLibrary(
